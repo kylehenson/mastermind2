733c6979cb32d1793dbc2e8311635481dbb110c4
Kyle Henson <khenson.kh@gmail.com>
2015-02-24 22:08:54 -0700

    Menu working on limitations on guess working. Still need to figure out counter.

--- lib/mastermind.rb
+++ lib/mastermind.rb
@@ -1,6 +1,8 @@
 require_relative 'response'
+require 'pry'
 
 class Mastermind
+  attr_accessor :number_of_guesses
 
   # def instructions
   #   Response.new(:message => "I have generated a random sequence of (r)ed, (b)lue, (g)reen, and (y)ellow. Guess the correct colors in the correct order:", :status => :continue)
@@ -9,12 +11,22 @@ class Mastermind
   def execute(input)
     secret = "BBGB"
     if input == "i" || input == "instructions"
-      Response.new(:message => "I have generated (r)ed, (b)lue, (g)reen, and (y)ellow in a random sequence. Guess the correct colors in the correct order:", :status => :continue)
+      Response.new(:message => "I will generate a random sequence of (r)ed, (b)lue, (g)reen, and (y)ellow in any order. Guess the correct colors in the correct order in as few attempts possible:", :status => :continue)
+    elsif input == "p" || input == "play"
+      Response.new(:message => "I have generated a beginner sequence with four elements made up of: (r)ed, (g)reen, (b)lue, and (y)ellow. Use (q)uit at any time to end the game. What's your guess?", :status => :continue)
     elsif input == "q" || input == "quit"
       Response.new(:message => "Thanks for playing!", :status => :stop)
+    elsif input == "c" || input == "cheat"
+      Response.new(:message => "The secret sequence is: #{secret}", :status => :continue)
+    elsif input.length < 4
+      Response.new(:message => "Are you paying attention?? You need to guess four colors. If you need instructions, enter 'i'.", :status => :continue)
+    elsif input.length > 4
+      Response.new(:message => "You are making this too complicated. Guess four colors only. If you need instructions, enter 'i'.", :status => :continue)
     elsif input == secret.downcase
-      Response.new(:message => "You Win!", :status => :won)
+      Response.new(:message => "Congratulations! You guessed the sequence '#{secret}' in #{@number_of_guesses} guess(es) in just #{Time.new}.\nWould you like to (p)lay again or (q)uit?", :status => :won)
+      # @number_of_guesses = 0
     else
+      # @number_of_guesses += 1
       Response.new(:message => "Guess again!", :status => :continue)
     end
   end
